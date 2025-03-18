e4cf782badecb8802c748637e902fe4ce0f45364
Ranjan kumar <ranjankumar5673@gmail.com>
2025-03-18 15:14:59 +0530

    feat: implement CASPathTransformFunc for content-addressed storage paths

--- store.go
+++ store.go
@@ -1,12 +1,53 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
 	"io"
+	"io/fs"
 	"log"
 	"os"
+	"strings"
 )
 
-type PathTransFormFunc func(string) string
+func CASPathTransformFunc(key string)PathKey{
+	hash := sha1.Sum([]byte(key))
+	hashStr := hex.EncodeToString(hash[:])
+
+	blocksize := 5
+	sliceLen  := len(hashStr)/blocksize
+	paths := make([]string,sliceLen)
+
+	for i:=0;i<sliceLen;i++{
+		from,to := i*blocksize,(i*blocksize)+blocksize
+		paths[i]=hashStr[from:to]
+	}
+	return PathKey{
+		PathName: strings.Join(paths,"/"),
+		Filename: hashStr,
+	}
+}
+
+type PathTransFormFunc func(string) PathKey
+
+type PathKey struct{
+	PathName   string
+	Filename   string
+}
+
+func(p PathKey)FirstPathName()string{
+	paths:= strings.Split(p.PathName,"/")
+	if len(paths)==0{
+		return ""
+	}
+	return paths[0]
+}
+
+func (p PathKey) FullPath()string{
+	return fmt.Sprintf("%s/%s",p.PathName,p.Filename)
+}
 
 type StoreOpts struct {
 	PathTransFormFunc PathTransFormFunc
@@ -26,22 +67,62 @@ func NewStore(opts StoreOpts) *Store {
 	}
 }
 
-func (s *Store) writeStream(key string, r io.Reader)error{
-	pathName:= s.PathTransFormFunc(key)
-	if err := os.MkdirAll(pathName,os.ModePerm);err!=nil{
-		return err
-	}
-	filename:= "somefilename"
-	pathAndFilename:= pathName + "/" + filename
-	f,err := os.Create(pathAndFilename)
-	if err!=nil{
-		return err
-	}
+func (s *Store) Has(key string)bool{
+	pathKey := s.PathTransFormFunc(key)
 
-	n,err := io.Copy(f,r)
-	if err!=nil{
-		return err
+	_,err := os.Stat(pathKey.FullPath())
+	if err == fs.ErrNotExist{
+		return false
 	}
-	log.Printf("written (%d) bytes to disk: %s",n,pathAndFilename)
-	return nil
+	return true
+}
+
+func (s *Store)Delete(key string)error{
+	pathKey := s.PathTransFormFunc(key)
+
+	defer func(){
+		log.Printf("deleted [%s] from disk",pathKey.Filename)
+	}()
+
+	return os.RemoveAll(pathKey.FirstPathName())
+}
+
+func (s *Store) Read(key string)(io.Reader,error){
+	f,err := s.readStream(key)
+	if err!=nil{
+		return nil,err
+	}
+	defer f.Close()
+	
+	buf := new(bytes.Buffer)
+	_,err = io.Copy(buf,f)
+
+	return buf,err
+}
+
+func (s *Store)readStream(key string)(io.ReadCloser,error){
+	PathKey := s.PathTransFormFunc(key)
+	return os.Open(PathKey.FullPath())
+}
+
+func (s *Store) writeStream(key string, r io.Reader)error{
+			pathKey:= s.PathTransFormFunc(key)
+			if err := os.MkdirAll(pathKey.PathName,os.ModePerm);err!=nil{
+				return err
+			}
+
+			fullPath:= pathKey.FullPath()
+
+			f,err := os.Create(fullPath)
+			if err!=nil{
+				return err
+			}
+			defer f.Close()
+
+			n,err := io.Copy(f,r)
+			if err!=nil{
+				return err
+			}
+			log.Printf("written (%d) bytes to disk: %s",n,fullPath)
+			return nil
 }
--- store_test.go
+++ store_test.go
@@ -2,16 +2,63 @@ package main
 
 import (
 	"bytes"
+	"io/ioutil"
 	"testing"
 )
 
-func TestStore(t *testing.T){
+func TestPathTransformFunc(t *testing.T){
+	key := "momsbestpicture"
+	pathKey:= CASPathTransformFunc(key)
+  expectedOriginalKey := "68044/29f74/181a6/3c50c/3d81d/733a1/2f14a/353ff"
+  expectedPathName := "68044/29f74/181a6/3c50c/3d81d/733a1/2f14a/353ff"
+	if pathKey.PathName !=expectedPathName{
+		t.Errorf("have %s want %s",pathKey.PathName,expectedPathName)
+	}
+	if pathKey.Filename!=expectedPathName{
+		t.Errorf("have %s want %s",pathKey.Filename,expectedOriginalKey)
+	}
+}
+
+func TestStoreDeleteKey(t *testing.T){
 	opts := StoreOpts{
-		PathTransFormFunc:DefaultPathTransformFunc ,
+		PathTransFormFunc:CASPathTransformFunc ,
 	}
 	s:= NewStore(opts)
-	data := bytes.NewReader([]byte("some jpg bytes"))
-	if err := s.writeStream("myspecialpicture",data);err!=nil{
+	key := "specialimages"
+	data := []byte("some jpg bytes")
+
+
+	if err := s.writeStream(key,bytes.NewReader(data));err!=nil{
 		t.Error(err)
 	}
+	if err := s.Delete(key);err!=nil{
+		t.Error(err)
+	}
+}
+
+func TestStore(t *testing.T){
+	opts := StoreOpts{
+		PathTransFormFunc:CASPathTransformFunc ,
+	}
+	s:= NewStore(opts)
+	key := "specialimages"
+	data := []byte("some jpg bytes")
+
+
+	if err := s.writeStream(key,bytes.NewReader(data));err!=nil{
+		t.Error(err)
+	}
+
+	r,err := s.Read(key)
+	if err!=nil{
+		t.Error(err)
+	}
+
+	b,_ := ioutil.ReadAll(r)
+
+	if string(b) !=string(data){
+		t.Errorf("want %s have %s",data,b)
+	}
+
+	s.Delete(key)
 }
